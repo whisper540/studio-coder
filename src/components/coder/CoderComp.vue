21b70f75e71a30cf0e7d3fd1b28e470b7f024c15
10002236 <wushidong@inovance.com>
2023-11-08 15:30:33 +0800

    init

---
+++ src/components/coder/CoderComp.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import {onMounted, ref} from 'vue';
+import type { Ref } from 'vue'
+import LogicFlow from "@logicflow/core";
+import "@logicflow/core/dist/style/index.css";
+import graphData from '@/components/coder/graph_data';
+
+const container:Ref<HTMLElement|null> = ref(null)
+const lf:Ref<LogicFlow|null> = ref(null);
+
+onMounted(()=>{
+    lf.value = new LogicFlow({
+        container: container.value!,
+        grid: true,
+    });
+    lf.value.render(graphData);
+})
+</script>
+
+<template>
+    <div class="container" ref="container"></div>
+</template>
+
+<style scoped>
+.container {
+    width: 1000px;
+    height: 800px;
+}
+</style>
---
+++ src/views/CoderView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import CoderComp from '../components/coder/CoderComp.vue'
+</script>
+
+<template>
+  <main>
+    <CoderComp />
+  </main>
+</template>
